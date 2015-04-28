6234b5cb5553f2aebe3875bd41ca55e082d15021
benj2240 <benj2240@gmail.com>
2015-04-27 22:57:31 -0500

    Replaced magical $ syntax with boring javascript.
    It doesn't work though.
    
    Should work, far as I can tell.
    
    I'm not a doctor.

--- LTVCalculation.js
+++ LTVCalculation.js
@@ -1,52 +1,30 @@
-$(document).ready(function(){
-    $("#calcFirstLoan").click(function(){
-        $("#FirstLoan").val(calcFirstLoan());
-    });
-    
-    $("#calcSecondLoan").click(function(){
-        $("#SecondLoan").val(calcSecondLoan());
-    });
-    
-    $("#calcAppraisal").click(function(){
-        $("#Appraisal").val(calcAppraisal());
-    });
-    
-    $("#calcLTV").click(function(){
-        $("#LTV").val(calcLTV());
-    });
-    
-    $("#calcCLTV").click(function(){
-        $("#CLTV").val(calcCLTV());
-    });
-    
-    function getFirstLoan() {
-        return $("#FirstLoan").val();
+document.addEventListener('DOMContentLoaded', function(){
+    // fetch values from the page
+    function getFirstLoan(){
+        return document.getElementByID("FirstLoan").value;
     }
-    
-    function getSecondLoan() {
-        return $("#SecondLoan").val();
+    function getSecondLoan(){
+        return document.getElementByID("SecondLoan").value;
     }
-    
-    function getAppraisal() {
-        return $("#Appraisal").val();
+    function getAppraisal(){
+        return document.getElementByID("Appraisal").value;
     }
-    
-    function getLTV() {
-        return $("#LTV").val();
+    function getLTV(){
+        return document.getElementByID("LTV").value;
     }
-    
-    function getCLTV() {
-        return $("#CLTV").val();
+    function getCLTV(){
+        return document.getElementByID("CLTV").value;
     }
-    
+
+    // derive values based on what is on the page
     function calcFirstLoan() {
         return getLTV()*getAppraisal();
     }
-    
+
     function calcSecondLoan() {
         return getCLTV()*getAppraisal()-getFirstLoan();
     }
-    
+
     function calcAppraisal() {
         if(getCLTV() != "" && getSecondLoan() != ""){
             // try to include second loan and CLTV
@@ -56,12 +34,36 @@ $(document).ready(function(){
             return getFirstLoan()/getLTV();
         }
     }
-    
+
     function calcLTV() {
         return getFirstLoan()/getAppraisal();
     }
-    
+
     function calcCLTV() {
         return (getFirstLoan()+getSecondLoan())/getAppraisal();
     }
+
+    // display values based on what was derived
+    function setFirstLoan(){
+        document.getElementByID("FirstLoan").value = calcFirstLoan();
+    }
+    function setSecondLoan(){
+        document.getElementByID("SecondLoan").value = calcSecondLoan();
+    }
+    function setAppraisal(){
+        document.getElementByID("Appraisal").value = calcAppraisal();
+    }
+    function setLTV(){
+        document.getElementByID("LTV").value = calcLTV();
+    }
+    function setCLTV(){
+        document.getElementByID("CLTV").value = calcCLTV();
+    }
+
+    // attach these functions to the page buttons
+    document.getElementByID("calcFirstLoan").onclick = setFirstLoan;
+    document.getElementByID("calcSecondLoan").onclick = setSecondLoan;
+    document.getElementByID("calcAppraisal").onclick = setAppraisal;
+    document.getElementByID("calcLTV").onclick = setLTV;
+    document.getElementByID("calcCLTV").onclick = setCLTV;
 });
