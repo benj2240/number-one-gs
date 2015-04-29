2db7a397b005dce13e08836062459f494d3c072c
benj2240 <benj2240@gmail.com>
2015-04-28 21:06:37 -0500

    Fixed syntax error; pure JS version now operational
    document.getElementByID != document.getElementById.

--- LTVCalculation.js
+++ LTVCalculation.js
@@ -1,69 +1,67 @@
-document.addEventListener('DOMContentLoaded', function(){
-    // fetch values from the page
-    function getFirstLoan(){
-        return document.getElementByID("FirstLoan").value;
-    }
-    function getSecondLoan(){
-        return document.getElementByID("SecondLoan").value;
-    }
-    function getAppraisal(){
-        return document.getElementByID("Appraisal").value;
-    }
-    function getLTV(){
-        return document.getElementByID("LTV").value;
-    }
-    function getCLTV(){
-        return document.getElementByID("CLTV").value;
-    }
+// fetch values from the page
+function getFirstLoan(){
+    return document.getElementById("FirstLoan").value;
+}
+function getSecondLoan(){
+    return document.getElementById("SecondLoan").value;
+}
+function getAppraisal(){
+    return document.getElementById("Appraisal").value;
+}
+function getLTV(){
+    return document.getElementById("LTV").value;
+}
+function getCLTV(){
+    return document.getElementById("CLTV").value;
+}
 
-    // derive values based on what is on the page
-    function calcFirstLoan() {
-        return getLTV()*getAppraisal();
-    }
+// derive values based on what is on the page
+function calcFirstLoan() {
+    return getLTV()*getAppraisal();
+}
 
-    function calcSecondLoan() {
-        return getCLTV()*getAppraisal()-getFirstLoan();
-    }
+function calcSecondLoan() {
+    return getCLTV()*getAppraisal()-getFirstLoan();
+}
 
-    function calcAppraisal() {
-        if(getCLTV() != "" && getSecondLoan() != ""){
-            // try to include second loan and CLTV
-            return (getFirstLoan()+getSecondLoan())/getCLTV();
-        }else{
-            // if one of those is blank, just use first loan and LTV
-            return getFirstLoan()/getLTV();
-        }
+function calcAppraisal() {
+    if(getCLTV() != "" && getSecondLoan() != ""){
+        // try to include second loan and CLTV
+        return (getFirstLoan()+getSecondLoan())/getCLTV();
+    }else{
+        // if one of those is blank, just use first loan and LTV
+        return getFirstLoan()/getLTV();
     }
+}
 
-    function calcLTV() {
-        return getFirstLoan()/getAppraisal();
-    }
+function calcLTV() {
+    return getFirstLoan()/getAppraisal();
+}
 
-    function calcCLTV() {
-        return (getFirstLoan()+getSecondLoan())/getAppraisal();
-    }
+function calcCLTV() {
+    return (getFirstLoan()+getSecondLoan())/getAppraisal();
+}
 
-    // display values based on what was derived
-    function setFirstLoan(){
-        document.getElementByID("FirstLoan").value = calcFirstLoan();
-    }
-    function setSecondLoan(){
-        document.getElementByID("SecondLoan").value = calcSecondLoan();
-    }
-    function setAppraisal(){
-        document.getElementByID("Appraisal").value = calcAppraisal();
-    }
-    function setLTV(){
-        document.getElementByID("LTV").value = calcLTV();
-    }
-    function setCLTV(){
-        document.getElementByID("CLTV").value = calcCLTV();
-    }
+// display values based on what was derived
+function setFirstLoan(){
+    document.getElementById("FirstLoan").value = calcFirstLoan();
+}
+function setSecondLoan(){
+    document.getElementById("SecondLoan").value = calcSecondLoan();
+}
+function setAppraisal(){
+    document.getElementById("Appraisal").value = calcAppraisal();
+}
+function setLTV(){
+    document.getElementById("LTV").value = calcLTV();
+}
+function setCLTV(){
+    document.getElementById("CLTV").value = calcCLTV();
+}
 
-    // attach these functions to the page buttons
-    document.getElementByID("calcFirstLoan").onclick = setFirstLoan;
-    document.getElementByID("calcSecondLoan").onclick = setSecondLoan;
-    document.getElementByID("calcAppraisal").onclick = setAppraisal;
-    document.getElementByID("calcLTV").onclick = setLTV;
-    document.getElementByID("calcCLTV").onclick = setCLTV;
-});
+// attach these functions to the page buttons
+document.getElementById("calcFirstLoan").onclick = setFirstLoan;
+document.getElementById("calcSecondLoan").onclick = setSecondLoan;
+document.getElementById("calcAppraisal").onclick = setAppraisal;
+document.getElementById("calcLTV").onclick = setLTV;
+document.getElementById("calcCLTV").onclick = setCLTV;
